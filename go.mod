d2f3d4fbe5dd7abe4e31b15d61ad96b1543e61f9
Alan Braithwaite <alan@runreveal.com>
2023-06-23 13:13:45 -0700

    wazero memory leak

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/runreveal/wazero_test
+
+go 1.20
+
+require github.com/tetratelabs/wazero v1.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"log"
+	"runtime"
+	"time"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+)
+
+// main is an example of how to extend a Go application with an addition
+// function defined in WebAssembly.
+
+//go:embed python-3.11.1.wasm
+var python []byte
+
+func main() {
+
+	for {
+		runme()
+		runtime.GC()
+		time.Sleep(time.Second)
+	}
+}
+
+func runme() {
+	ctx := context.Background()
+	rConfig := wazero.NewRuntimeConfig().
+		WithMemoryLimitPages(32768).
+		WithCloseOnContextDone(true)
+	r := wazero.NewRuntimeWithConfig(ctx, rConfig)
+
+	apiCloser, err := wasi_snapshot_preview1.Instantiate(ctx, r)
+	mod, err := r.CompileModule(ctx, python)
+	if err != nil {
+		log.Panicf("failed to instantiate module: %v", err)
+	}
+	fmt.Println(mod)
+	err = apiCloser.Close(ctx)
+	if err != nil {
+		log.Panicf("failed to close module: %v", err)
+	}
+	err = r.Close(ctx)
+	if err != nil {
+		log.Panicf("failed to close runtime: %v", err)
+	}
+}
